db7507d4555a893343de35a91dd6b9fb295ed4b5
Henrik Plate <17928867+henrikplate@users.noreply.github.com>
2018-10-26 17:14:07 +0200

    shared: Fixed deletion of cache entries when maxSize==-1; added test class

--- shared/src/main/java/com/sap/psr/vulas/shared/cache/Cache.java
+++ shared/src/main/java/com/sap/psr/vulas/shared/cache/Cache.java
@@ -20,7 +20,7 @@ public class Cache<S, T> {
 	private static final Log log = LogFactory.getLog(Cache.class);
 
 	private long refreshMilli = -1;
-	private int max_size = -1;
+	private int maxSize = -1;
 	private ObjectFetcher<S, T> reader = null;
 	private Map<S,CacheEntry<T>> entries = new HashMap<S,CacheEntry<T>>();
 	
@@ -43,8 +43,7 @@ public class Cache<S, T> {
 	 * @param _refresh_min
 	 */
 	public Cache(ObjectFetcher<S, T> _reader, long _refresh_min) {
-		this.refreshMilli = _refresh_min * MILLI_IN_MIN;
-		this.reader = _reader;
+		this(_reader, _refresh_min, -1);
 	}
 	
 	/**
@@ -54,7 +53,7 @@ public class Cache<S, T> {
 	public Cache(ObjectFetcher<S, T> _reader, long _refresh_min, int _max_size) {
 		this.reader = _reader;
 		this.refreshMilli = _refresh_min * MILLI_IN_MIN;
-		this.max_size = _max_size;
+		this.maxSize = _max_size;
 	}
 	
 	public synchronized T get(S _key) throws CacheException {
@@ -69,23 +68,7 @@ public class Cache<S, T> {
 	 */
 	public synchronized T get(S _key, boolean _force_fetch) throws CacheException {
 		final long current_time = System.currentTimeMillis();
-		
-		if(this.entries.size()>this.max_size){
-			S to_del=null;
-			long ageMilli =-1;
-			for(Entry<S, CacheEntry<T>> e:this.entries.entrySet()){
-				if(current_time-e.getValue().getCreatedAt()>ageMilli){
-					ageMilli=current_time - e.getValue().getCreatedAt();
-					to_del=e.getKey();
-				}
-			}
-			if(to_del!=null){
-				log.info("Removing key [ "+ to_del.toString()  +"] from cache.");
-				this.cacheDeleteCount++;
-				this.entries.remove(to_del);
-			}
-		}
-				
+						
 		// Print stats
 		if(this.cacheRequestCount%PRINT_STATS_EVERY==0)
 			this.printStats();
@@ -105,7 +88,7 @@ public class Cache<S, T> {
 			this.cacheMissCount++;
 		}
 		
-		// (Re)read object if necessary
+		// Fetch object (if necessary)
 		if(e==null) {
 			this.cacheFetchCount++;
 			final StopWatch sw = new StopWatch("Fetch cache entry for key [" + _key + "]").start();
@@ -120,6 +103,27 @@ public class Cache<S, T> {
 				cacheFetchErrorCount++;
 				throw e1;
 			}
+			
+			// Delete oldest entry if max cache size reached
+			if(this.maxSize!=-1 && this.entries.size() > this.maxSize) {
+				S to_del=null;
+				long age_milli =-1;
+				
+				// Find oldest entry (can be done in a more performant way)
+				for(Entry<S, CacheEntry<T>> e2:this.entries.entrySet()){
+					if(current_time-e2.getValue().getCreatedAt()>age_milli){
+						age_milli=current_time - e2.getValue().getCreatedAt();
+						to_del=e2.getKey();
+					}
+				}
+				
+				// Delete
+				if(to_del!=null){
+					this.cacheDeleteCount++;
+					this.entries.remove(to_del);
+					log.info("Removed key [ "+ to_del.toString()  +"] from cache due to cache size limit of [" + this.maxSize + "]");
+				}
+			}
 		}
 		
 		if(e==null)
@@ -127,6 +131,12 @@ public class Cache<S, T> {
 		
 		return e.getObject();
 	}
+
+	public long getCacheRequest() { return this.cacheRequestCount; }
+	public long getCacheHit() { return this.cacheHitCount; }
+	public long getCacheMiss() { return this.cacheMissCount; }
+	public long getCacheFetch() { return this.cacheFetchCount; }
+	public long getCacheDelete() { return this.cacheDeleteCount; }
 	
 	private class CacheEntry<U> {
 		
