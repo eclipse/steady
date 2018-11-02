7f43527441345338a79995470e818d69f2e5cbd1
Naramsim <igougi.ui@gmail.com>
2018-11-02 16:27:52 +0100

    chore(travis): fix check/better lifecycle

--- .travis/check.sh
+++ .travis/check.sh
@@ -5,13 +5,11 @@ set -x
 
 sleep 15
 
-N_CONTAINERS_RUNNING=$(docker ps --filter "status=running" -q | wc -l)
+N_CONTAINERS_RUNNING=$(docker ps --filter "status=running" | grep vulas | awk '{print $1}' | wc -l)
 
 if [ ${N_CONTAINERS_RUNNING} -eq 7 ]
 then
     exit 0
 else
-    docker images
-    docker ps -as
     exit 1
 fi
