e39280c0bacc9f9583d7c20ae2751f6c4dd82389
diffblue-assistant <contributions@diffblue.com>
2018-11-21 08:03:35 +0000

    Add unit tests to com.sap.psr.vulas.java.JavaId These tests were written by Diffblue Cover.

--- lang-java/src/test/java/com/sap/psr/vulas/java/JavaIdTest.java
+++ lang-java/src/test/java/com/sap/psr/vulas/java/JavaIdTest.java
@@ -5,9 +5,14 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
+import com.sap.psr.vulas.ConstructId;
 import com.sap.psr.vulas.java.JavaClassId;
 import com.sap.psr.vulas.java.JavaId;
 import com.sap.psr.vulas.java.JavaMethodId;
@@ -18,6 +23,8 @@ import com.sap.psr.vulas.java.JavaPackageId;
  *
  */
 public class JavaIdTest {
+
+	@Rule public ExpectedException thrown = ExpectedException.none();
 
 	@Test 
 	public void packageIdTest() {
@@ -80,6 +87,153 @@ public class JavaIdTest {
 		assertTrue(JavaId.removePackageContext("a.b.c.Class").equals("Class"));
 		assertTrue(JavaId.removePackageContext("a.b.c.Class$NestedClass").equals("NestedClass"));
 		assertTrue(JavaId.removePackageContext("Class").equals("Class"));
+	}
+
+	@Test
+	public void typeFromStringInputNotNullOutputIllegalArgumentException() {
+  
+	  // Arrange
+	  final String _t = "";
+  
+	  // Act
+	  thrown.expect(IllegalArgumentException.class);
+	  JavaId.typeFromString(_t);
+  
+	  // Method is not expected to return due to exception thrown
+	}
+
+	@Test
+	public void typeFromStringInputNotNullOutputNotNull() {
+  
+	  // Arrange
+	  final String _t = "ENUM";
+  
+	  // Act
+	  final JavaId.Type retval = JavaId.typeFromString(_t);
+  
+	  // Assert result
+	  assertEquals(JavaId.Type.ENUM, retval);
+	}
+
+	@Test
+	public void typeFromStringInputNotNullOutputNotNull2() {
+  
+	  // Arrange
+	  final String _t = "pACK";
+  
+	  // Act
+	  final JavaId.Type retval = JavaId.typeFromString(_t);
+  
+	  // Assert result
+	  assertEquals(JavaId.Type.PACKAGE, retval);
+	}
+
+	@Test
+	public void typeFromStringInputNotNullOutputNotNull3() {
+  
+	  // Arrange
+	  final String _t = "iNIT";
+  
+	  // Act
+	  final JavaId.Type retval = JavaId.typeFromString(_t);
+  
+	  // Assert result
+	  assertEquals(JavaId.Type.CLASSINIT, retval);
+	}
+
+	@Test
+	public void typeFromStringInputNotNullOutputNotNull4() {
+  
+	  // Arrange
+	  final String _t = "CLAS";
+  
+	  // Act
+	  final JavaId.Type retval = JavaId.typeFromString(_t);
+  
+	  // Assert result
+	  assertEquals(JavaId.Type.CLASS, retval);
+	}
+
+	@Test
+	public void typeFromStringInputNotNullOutputNotNull5() {
+  
+	  // Arrange
+	  final String _t = "InTF";
+  
+	  // Act
+	  final JavaId.Type retval = JavaId.typeFromString(_t);
+  
+	  // Assert result
+	  assertEquals(JavaId.Type.INTERFACE, retval);
+	}
+
+	@Test
+	public void typeFromStringInputNotNullOutputNotNull6() {
+  
+	  // Arrange
+	  final String _t = "CoNs";
+  
+	  // Act
+	  final JavaId.Type retval = JavaId.typeFromString(_t);
+  
+	  // Assert result
+	  assertEquals(JavaId.Type.CONSTRUCTOR, retval);
+	}
+
+	@Test
+	public void typeFromStringInputNotNullOutputNotNull7() {
+  
+	  // Arrange
+	  final String _t = "NCla";
+  
+	  // Act
+	  final JavaId.Type retval = JavaId.typeFromString(_t);
+  
+	  // Assert result
+	  assertEquals(JavaId.Type.NESTED_CLASS, retval);
+	}
+
+	@Test
+	public void typeFromStringInputNotNullOutputNotNull8() {
+  
+	  // Arrange
+	  final String _t = "MEtH";
+  
+	  // Act
+	  final JavaId.Type retval = JavaId.typeFromString(_t);
+  
+	  // Assert result
+	  assertEquals(JavaId.Type.METHOD, retval);
+	}
+
+	@Test
+	public void filterInputNotNull0Output0() {
+  
+	  // Arrange
+	  final HashSet _set = new HashSet();
+	  final JavaId.Type[] _filter = {};
+  
+	  // Act
+	  final Set<ConstructId> retval = JavaId.filter(_set, _filter);
+  
+	  // Assert result
+	  final HashSet<ConstructId> hashSet = new HashSet<ConstructId>();
+	  assertEquals(hashSet, retval);
+	}
+
+	@Test
+	public void filterInputNotNullNotNullOutput0() {
+  
+	  // Arrange
+	  final HashSet _set = new HashSet();
+	  final String _filter = "";
+  
+	  // Act
+	  final Set<ConstructId> retval = JavaId.filter(_set, _filter);
+  
+	  // Assert result
+	  final HashSet<ConstructId> hashSet = new HashSet<ConstructId>();
+	  assertEquals(hashSet, retval);
 	}
 
 }
