3fe07242a81e5d37700734951ded6eba047593aa
Naramsim <igougi.ui@gmail.com>
2019-01-09 10:56:01 +0100

    feat(docs): accept URL parameter

--- docs/checklinks.sh
+++ docs/checklinks.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-muffet -e .*/edit/.* -e .*/f2895a6e-ca7c-0010-82c7-eda71af511fa.html -e .*exploit-db\.com -e .*corp[/:].* -e .*:8033/.* -e .*/apps.* -e .*/bugs.* -e .*maven\.apache\.org.* -e .*docs\.oracle\.com.* -e .*wala\.sourceforge\.net.* -t 20 http://127.0.0.1:8000
+URL=${1:-127.0.0.1:8000}
+
+muffet -e .*/edit/.* -e .*/f2895a6e-ca7c-0010-82c7-eda71af511fa.html -e .*exploit-db\.com -e .*corp[/:].* -e .*:8033/.* -e .*/apps.* -e .*/bugs.* -e .*maven\.apache\.org.* -e .*docs\.oracle\.com.* -e .*wala\.sourceforge\.net.* -t 20 http://$URL
