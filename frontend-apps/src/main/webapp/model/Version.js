720b34431e0df78e3fcd7c4fb1e323973c29796f
Naramsim <igougi.ui@gmail.com>
2019-01-21 12:37:01 +0100

    fix(frontend-apps): remove Version.js

--- frontend-apps/src/main/webapp/model/Version.js
+++
@@ -1,9 +0,0 @@
-jQuery.sap.declare("model.Version");
-
-// Placeholders replaced during Maven build
-model.Version = {
-		version:"$version$",
-		buildTimestamp:"$buildTimestamp$",
-		buildNumber:"$buildNumber$",
-		buildBranch:"$buildScmBranch$"
-};
