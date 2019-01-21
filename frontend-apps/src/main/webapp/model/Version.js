0182cddd2ce5aaa2504a7cdd5b937d283ffa985b
Naramsim <igougi.ui@gmail.com>
2019-01-21 12:34:53 +0100

    feat(travis): comment some tests

---
+++ frontend-apps/src/main/webapp/model/Version.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("model.Version");
+
+// Placeholders replaced during Maven build
+model.Version = {
+		version:"$version$",
+		buildTimestamp:"$buildTimestamp$",
+		buildNumber:"$buildNumber$",
+		buildBranch:"$buildScmBranch$"
+};
