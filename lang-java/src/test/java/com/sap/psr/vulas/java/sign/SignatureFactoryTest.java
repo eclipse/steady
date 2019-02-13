c82bfca1f9cd4575adeae2b58058f40877630b7b
Serena <serena.ponta@sap.com>
2019-02-13 10:45:35 +0100

    updated guava

--- lang-java/src/test/java/com/sap/psr/vulas/java/sign/SignatureFactoryTest.java
+++ lang-java/src/test/java/com/sap/psr/vulas/java/sign/SignatureFactoryTest.java
@@ -16,7 +16,7 @@ public class SignatureFactoryTest {
     @Test
     public void createSignature() throws Exception {
     	JavaSignatureFactory f = new JavaSignatureFactory();
-    	ASTConstructBodySignature s = (ASTConstructBodySignature) f.createSignature(new ConstructId(ProgrammingLanguage.JAVA, ConstructType.METH, "org.apache.catalina.filters.CorsFilter.handleSimpleCORS(HttpServletRequest,HttpServletResponse,FilterChain)"), new File("./src/test/resources/CorsFilter.java"));
+    	ASTConstructBodySignature s = (ASTConstructBodySignature) f.createSignature(new ConstructId(ProgrammingLanguage.JAVA, ConstructType.METH, "org.foo.Filter.doFilter(Boolean,String,String)"), new File("./src/test/resources/Filter.java"));
     	assertTrue(s.toJson().length()>0);
     	
     }
