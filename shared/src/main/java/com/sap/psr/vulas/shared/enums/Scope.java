d86e2b65a8724a795d6d34f0a123cc063e1104e5
CeDa <cedric.dangremont@sap.com>
2019-03-12 16:47:07 +0100

    using RUNTIME as default scope when a custom/non standard scope is provided

--- shared/src/main/java/com/sap/psr/vulas/shared/enums/Scope.java
+++ shared/src/main/java/com/sap/psr/vulas/shared/enums/Scope.java
@@ -12,32 +12,31 @@ import org.apache.commons.logging.LogFactory;
  * Use in com.sap.psr.vulas.backend.model.Dependency and com.sap.psr.vulas.shared.json.model.Dependency.
  */
 public enum Scope {
-	COMPILE, PROVIDED, RUNTIME, TEST, SYSTEM, IMPORT, CUSTOM;
+	COMPILE, PROVIDED, RUNTIME, TEST, SYSTEM, IMPORT;
 
 	private static final Log log = LogFactory.getLog(Scope.class);
 	
 	public static final Set<Scope> fromStringArray(String[] _values) {
 		final Set<Scope> scopes = new HashSet<Scope>();
 		if(_values!=null) {
-			for(String v: _values) {
+			for(String v: _values) {				
 				try {
-					scopes.add(Scope.valueOf(v.toUpperCase()));
-				} catch (IllegalArgumentException iae) {
-					Scope.log.warn("Undefined maven scope " + v.toUpperCase() + " was found in configuration. Changing it to COMPILE");
-					scopes.add (Scope.COMPILE);
-				}catch (Exception e) {}
+					scopes.add(Scope.valueOf(v));
+				} catch(IllegalArgumentException e) {
+					Scope.log.warn("Invalid scope [" + v + "] ignored, use one of [COMPILE, PROVIDED, RUNTIME, TEST, SYSTEM, IMPORT]");
+				}
 			}
 		}
 		return scopes;
 	}
 
 
-	public static Scope fromString (String _value) {
+	public static Scope fromString(String _value, Scope _default) {
 		try {
 			return Scope.valueOf(_value.toUpperCase());
 		} catch (IllegalArgumentException iae) {
-			Scope.log.warn("Undefined maven scope " + _value.toUpperCase() + " was found in configuration. Changing it to COMPILE");
-			return Scope.COMPILE;
+			Scope.log.warn("Invalid scope [" + _value + "], returning default [" + _default + "]");
+			return _default;
 		}
     }
     
