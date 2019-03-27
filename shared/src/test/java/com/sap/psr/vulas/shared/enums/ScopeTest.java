ca0cb97844d972aa805de731062c459a8462ba3e
Henrik Plate <17928867+henrikplate@users.noreply.github.com>
2019-03-27 12:49:04 +0100

    Added ScopeTest

---
+++ shared/src/test/java/com/sap/psr/vulas/shared/enums/ScopeTest.java
@@ -0,0 +1,20 @@
+package com.sap.psr.vulas.shared.enums;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
+
+import org.junit.Test;
+
+public class ScopeTest {
+
+	@Test
+	public void testFromStringArray() {
+		final String[] scope_strings = new String[] { "teST", "compiLE" };
+		final Set<Scope> scopes = Scope.fromStringArray(scope_strings);
+		assertTrue(scopes.contains(Scope.TEST));
+		assertTrue(scopes.contains(Scope.COMPILE));
+		assertEquals(2, scopes.size());
+	}
+}
