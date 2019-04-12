e82ae6eb396962e2cdfd9ec295dac52ee9cbf613
Naramsim <igougi.ui@gmail.com>
2019-04-12 16:52:15 +0200

    feat(docker): hare more jars to the host machine

--- docker/run.sh
+++ docker/run.sh
@@ -15,7 +15,8 @@ rm /exporter/**/*.?ar 2> /dev/null
 rm /exporter/client-components/*.?ar 2> /dev/null
 
 VULAS_JAVA_BACKEND_COMPONENTS="frontend-apps frontend-bugs patch-lib-analyzer rest-backend rest-lib-utils"
-VULAS_JAVA_CLIENT_COMPONENTS="patch-analyzer cli-scanner"
+VULAS_JAVA_CLIENT_COMPONENTS="patch-analyzer cli-scanner plugin-maven plugin-gradle"
+VULAS_JAVA_COMPONENTS="cli-scanner frontend-apps frontend-bugs lang-java-reach-wala lang-java-reach lang-java lang-python lang patch-analyzer patch-lib-analyzer plugin-maven repo-client rest-backend rest-lib-utils shared "
 
 echo -e '\n[+] Copying new archives'
 
@@ -27,6 +28,12 @@ mkdir -p /exporter/client-components/
 
 for i in $VULAS_JAVA_CLIENT_COMPONENTS ; do
     cp $i/target/*.?ar /exporter/client-components/
+done
+
+mkdir -p /exporter/all-components/
+
+for i in $VULAS_JAVA_COMPONENTS ; do
+    cp $i/target/*.?ar /exporter/all-components/
 done
 
 echo -e '\n[+] Done'
