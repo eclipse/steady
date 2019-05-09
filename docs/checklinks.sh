a7cbf3595cbf636908f2676aae2655414d45f36c
Alessandro Pezzè <igougi.ui@gmail.com>
2019-05-09 09:52:35 +0200

    chore: filter out stackoverflow

--- docs/checklinks.sh
+++ docs/checklinks.sh
@@ -6,4 +6,4 @@
 
 URL=${1:-127.0.0.1:8000}
 
-muffet -e .*/edit/.* -e .*/f2895a6e-ca7c-0010-82c7-eda71af511fa.html -e .*exploit-db\.com -e .*corp[/:].* -e .*:8033/.* -e .*/apps.* -e .*/bugs.* -e .*maven\.apache\.org.* -e .*docs\.oracle\.com.* -e .*wala\.sourceforge\.net.* -t 20 http://$URL
+muffet -e .*/edit/.* -e .*/f2895a6e-ca7c-0010-82c7-eda71af511fa.html -e .*exploit-db\.com -e .*corp[/:].* -e .*:8033/.* -e .*/apps.* -e .*/bugs.* -e .*maven\.apache\.org.* -e .*docs\.oracle\.com.* -e .*wala\.sourceforge\.net.* -e .*stackoverflow\.com.* -t 20 http://$URL
