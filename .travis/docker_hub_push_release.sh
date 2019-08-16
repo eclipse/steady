962567845cc65ba7f0ae220e01cec2e48f52948b
Naramsim <igougi.ui@gmail.com>
2019-08-16 13:46:37 +0200

    fix(travis): use bash when pushing images

--- .travis/docker_hub_push_release.sh
+++ .travis/docker_hub_push_release.sh
@@ -4,7 +4,7 @@
 
 if [ "$VULAS_RELEASE" == "$TRAVIS_TAG" ]; then
     echo "$DOCKER_HUB_NARAMSIM_PASSWORD" | docker login -u "$DOCKER_HUB_NARAMSIM_USERNAME" --password-stdin
-    (cd docker && ./push-images.sh -r registry.hub.docker.com -p vulas -v "$TRAVIS_TAG")
+    (cd docker && bash push-images.sh -r registry.hub.docker.com -p vulas -v "$TRAVIS_TAG")
 else
     echo '[-] VULAS_RELEASE and Git tag mismatch'
     echo "    VULAS_RELEASE: ${VULAS_RELEASE}"
