d3e10c5ed18fdad3d2eeffb15088b9c16a52b536
Naramsim <igougi.ui@gmail.com>
2019-08-16 13:20:46 +0200

    fix(travis): add USERNAME env var/adjust script locations

--- .travis/docker_hub_push_snapshot.sh
+++ .travis/docker_hub_push_snapshot.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
-set -x
+# Should be run from repository's root
+# Usage: bash .travis/docker_hub_push_snapshot.sh
 
 if [[ $VULAS_RELEASE =~ ^([0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT)$ ]]; then
     echo "$DOCKER_HUB_NARAMSIM_PASSWORD" | docker login -u "$DOCKER_HUB_NARAMSIM_USERNAME" --password-stdin
-    (cd ../docker && ./push-images.sh -r registry.hub.docker.com -p vulas -v "${VULAS_RELEASE}")
+    (cd docker && ./push-images.sh -r registry.hub.docker.com -p vulas -v "${VULAS_RELEASE}")
 else
     echo '[!] Refusing to push non-snapshot version'
     echo "    VULAS_RELEASE: $VULAS_RELEASE"
