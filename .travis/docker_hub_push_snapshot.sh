b868635c23b8f0100c1bcca7c833ed3a7f63da46
Naramsim <igougi.ui@gmail.com>
2019-08-16 12:05:46 +0200

    fix(travis): adjust scripts locations

--- .travis/docker_hub_push_snapshot.sh
+++ .travis/docker_hub_push_snapshot.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -x
+
 if [[ $VULAS_RELEASE =~ ^([0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT)$ ]]; then
     echo "$DOCKER_HUB_NARAMSIM_PASSWORD" | docker login -u "$DOCKER_HUB_NARAMSIM_USERNAME" --password-stdin
-    (cd docker && push-images.sh -r registry.hub.docker.com -p vulas -v "${VULAS_RELEASE}")
+    (cd ../docker && ./push-images.sh -r registry.hub.docker.com -p vulas -v "${VULAS_RELEASE}")
 else
     echo '[!] Refusing to push non-snapshot version'
     echo "    VULAS_RELEASE: $VULAS_RELEASE"
