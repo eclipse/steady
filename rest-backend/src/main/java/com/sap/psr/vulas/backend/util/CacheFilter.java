a08b57132c41c1730318c2467b3b2218b5bc988a
Naramsim <igougi.ui@gmail.com>
2019-09-17 09:35:17 +0200

    docs(rest-backend): add Javadoc

--- rest-backend/src/main/java/com/sap/psr/vulas/backend/util/CacheFilter.java
+++ rest-backend/src/main/java/com/sap/psr/vulas/backend/util/CacheFilter.java
@@ -12,12 +12,29 @@ import java.io.IOException;
 
 // Component duplicated in rest-lib-utils/src/main/java/com/sap/psr/vulas/backend/util/CacheFilter.java
 
+/**
+ * <p>CacheFilter class.</p>
+ *
+ */
 @Component
 public class CacheFilter implements Filter {
+
+    /**
+    * Default destroy
+    * @return void
+    */
     @Override
     public void destroy() {
         // Nothing
     }
+
+    /**
+    * Appends to the response a X-Accel-Expires header equal to two hours if cache=true is present in the querystring of the request
+    * @param  request  an incoming HTTP request
+    * @param  response the HTTP resonse to return
+    * @param  chain the chain of filters
+    * @return void
+    */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String cache = request.getParameter("cache");
@@ -28,6 +45,12 @@ public class CacheFilter implements Filter {
         }
         chain.doFilter(request, response);
     }
+
+    /**
+    * Default init
+    * @param  filterConfig the configuration of the filter
+    * @return void
+    */
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         // Nothing
