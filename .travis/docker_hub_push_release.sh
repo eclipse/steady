ea0f3c3889403f13b85de87ceb771f16f6b82dcc
Hoang Quoc Trung <quoctrunghoang1998@gmail.com>
2019-10-17 00:47:26 -0700

    fix(travis): add jib job to openjdk8 block

--- .travis/docker_hub_push_release.sh
+++ .travis/docker_hub_push_release.sh
@@ -5,7 +5,6 @@
 if [ "$VULAS_RELEASE" == "$TRAVIS_TAG" ]; then
     echo "$DOCKER_HUB_NARAMSIM_PASSWORD" | docker login -u "$DOCKER_HUB_NARAMSIM_USERNAME" --password-stdin
     (cd docker && bash push-images.sh -r docker.io -p vulas -v "$TRAVIS_TAG")
-    ./.travis/skaffold build -f ./.travis/skaffold.yaml
 else
     echo '[-] VULAS_RELEASE and Git tag mismatch'
     echo "    VULAS_RELEASE: ${VULAS_RELEASE}"
