628abe1060254ccce76e8206cab618c1e5d6fb96
Hoang Quoc Trung <quoctrunghoang1998@gmail.com>
2019-10-18 07:09:56 -0700

    fix(travis): fix script provider in openjdk travis block

--- .travis/docker_hub_push_release.sh
+++ .travis/docker_hub_push_release.sh
@@ -2,12 +2,13 @@
 # Should be run from repository's root
 # Usage: bash .travis/docker_hub_push_releases.sh
 
-if [ "$VULAS_RELEASE" != "$TRAVIS_TAG" ]; then
-  echo '[-] VULAS_RELEASE and Git tag mismatch'
-  echo "    VULAS_RELEASE: ${VULAS_RELEASE}"
-  echo "    Git tag: ${TRAVIS_TAG}"
-  exit 1
-fi
+# 
+# if [ "$VULAS_RELEASE" != "$TRAVIS_TAG" ]; then
+#   echo '[-] VULAS_RELEASE and Git tag mismatch'
+#   echo "    VULAS_RELEASE: ${VULAS_RELEASE}"
+#   echo "    Git tag: ${TRAVIS_TAG}"
+#   exit 1
+# fi
 
 echo "$DOCKER_HUB_NARAMSIM_PASSWORD" | docker login -u "$DOCKER_HUB_NARAMSIM_USERNAME" --password-stdin
 if [ -z "$JIB" ]; then
