88bd7ae1c719ce7b75134d7a6b07a2c47b3d1b9f
Hoang Quoc Trung <quoctrunghoang1998@gmail.com>
2019-10-30 07:39:25 -0700

    fix(lang-java-reach-soot): address RC_REF_COMPARISON

--- lang-java-reach/src/main/java/com/sap/psr/vulas/cg/PrunedGraphGetPaths.java
+++ lang-java-reach/src/main/java/com/sap/psr/vulas/cg/PrunedGraphGetPaths.java
@@ -18,13 +18,13 @@ import com.sap.psr.vulas.shared.json.model.ConstructId;
  *
  */
 public class PrunedGraphGetPaths extends AbstractGetPaths {
-	
+
 	private static final Log log = LogFactory.getLog(PrunedGraphGetPaths.class);
-	
-	private long start_millis = System.currentTimeMillis(), end_millis = System.currentTimeMillis();	
+
+	private long start_millis = System.currentTimeMillis(), end_millis = System.currentTimeMillis();
 
 	private Map<Integer, HashSet<Integer>> edges = new HashMap<Integer, HashSet<Integer>>();
-	
+
 	/**
 	 * <p>Constructor for PrunedGraphGetPaths.</p>
 	 *
@@ -55,7 +55,7 @@ public class PrunedGraphGetPaths extends AbstractGetPaths {
 	}
 
 	private void assemblePath(Integer _src, Integer _tgt, LinkedList<Integer> _path, HashSet<LinkedList<Integer>> _allpaths) {
-		if (_src == _tgt) {
+		if (_src.equals(_tgt)) {
 			LinkedList<Integer> newPath = new LinkedList<Integer>();
 			newPath.addAll(_path);
 			_allpaths.add(newPath);
@@ -79,11 +79,11 @@ public class PrunedGraphGetPaths extends AbstractGetPaths {
 		if (_path != null && !_path.isEmpty())
 			_path.removeLast();
 	}
-	
+
 	/**
 	 * Prune the callgraph, make it only contain all edges heading to the target
 	 * (_tgt)
-	 * 
+	 *
 	 * @param _tgt
 	 * @return
 	 */
@@ -93,7 +93,7 @@ public class PrunedGraphGetPaths extends AbstractGetPaths {
 		// Intialization: no edge at all in the pruned callgraph
 		while (nodes.hasNext()) {
 			node = nodes.next();
-			if (node == _tgt) {
+			if (node.equals(_tgt)) {
 				this.edges.put(node, new HashSet<Integer>());
 			} else
 				this.edges.put(node, null);
@@ -103,7 +103,7 @@ public class PrunedGraphGetPaths extends AbstractGetPaths {
 
 	/**
 	 * Recursive method of getAllEdges, update the Map _edges
-	 * 
+	 *
 	 * @param _tgt
 	 * @param _edges
 	 * @return
@@ -123,5 +123,5 @@ public class PrunedGraphGetPaths extends AbstractGetPaths {
 				computeAllEdges(prednode);
 			}
 		}
-	}	
+	}
 }
