bad0ea06493e73b7cd665aa41427b06387edb715
Henrik Plate <17928867+henrikplate@users.noreply.github.com>
2019-12-30 10:08:35 +0100

    Fixed: Get CVE ID from alternative bug identifier

--- rest-backend/src/main/java/com/sap/psr/vulas/backend/repo/BugRepositoryImpl.java
+++ rest-backend/src/main/java/com/sap/psr/vulas/backend/repo/BugRepositoryImpl.java
@@ -146,7 +146,14 @@ public class BugRepositoryImpl implements BugRepositoryCustom {
 			   ( _bug.getDescription()==null || _bug.getCvssScore()==null || _bug.getCvssVersion()==null );
 	}
 
-	/** {@inheritDoc} */
+	/**
+	 * Checks whether the database has a description, CVSS score and version for the given {@link Bug}.
+	 * If not, or in case the parameter _force is true, the CVE data for the given {@link Bug} is read using the {@link CveReader2},
+	 * and the database is updated if the CVE's summary, CVSS score, version or vector is empty or outdated.
+	 * 
+	 * @param _b the {@link Bug} whose CVE data is read
+	 * @param _force 
+	 */
 	@Override
 	public boolean updateCachedCveData(Bug _b, boolean _force) {
 		boolean update_happened = false;
@@ -155,31 +162,30 @@ public class BugRepositoryImpl implements BugRepositoryCustom {
 				// Get the CVE ID to read from cache
 				String cve_id = Cve.extractCveIdentifier(_b.getBugId());
 				if(cve_id==null)
-					Cve.extractCveIdentifier(_b.getBugIdAlt());
+					cve_id = Cve.extractCveIdentifier(_b.getBugIdAlt());
 				
 				// Read cache (note that cve_id can be null)
 				final Cve cve = CveReader2.read(cve_id);
 				
 				if(cve!=null) {
 					boolean to_save=false; 
-					if(cve.getSummary()!=null && (_b.getDescription()==null || !(cve.getSummary().equals(_b.getDescription())))){
+					if(cve.getSummary()!=null && (_b.getDescription()==null || !(cve.getSummary().equals(_b.getDescription())))) {
 						_b.setDescription(cve.getSummary());
-						to_save=true;
-						
+						to_save = true;						
 					}
-					if(cve.getCvssScore()!=null && (_b.getCvssScore()==null || !(cve.getCvssScore().equals(_b.getCvssScore())))){
+					if(cve.getCvssScore()!=null && (_b.getCvssScore()==null || !(cve.getCvssScore().equals(_b.getCvssScore())))) {
 						_b.setCvssScore(cve.getCvssScore());
-						to_save=true;
+						to_save = true;
 					}
-					if(cve.getCvssVersion()!=null && (_b.getCvssVersion()==null || !(cve.getCvssVersion().equals(_b.getCvssVersion())))){
+					if(cve.getCvssVersion()!=null && (_b.getCvssVersion()==null || !(cve.getCvssVersion().equals(_b.getCvssVersion())))) {
 						_b.setCvssVersion(cve.getCvssVersion());
-						to_save=true;
+						to_save = true;
 					}
-					if(cve.getCvssVector()!=null && (_b.getCvssVector()==null || !(cve.getCvssVector().equals(_b.getCvssVector())))){
+					if(cve.getCvssVector()!=null && (_b.getCvssVector()==null || !(cve.getCvssVector().equals(_b.getCvssVector())))) {
 						_b.setCvssVector(cve.getCvssVector());
-						to_save=true;
+						to_save = true;
 					}
-					if(to_save){
+					if(to_save) {
 						this.customSave(_b, false);
 						update_happened = true;
 					}
