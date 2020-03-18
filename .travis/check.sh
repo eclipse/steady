14219144d81ca91dd89abce57f4823d692b77ecb
Sumeet Patil <sumeet.patil@sap.com>
2020-03-18 13:56:13 +0100

    integrate rest-nvd
    Signed-off-by: Sumeet Patil <sumeet.patil@sap.com>

--- .travis/check.sh
+++ .travis/check.sh
@@ -8,7 +8,7 @@ sleep 15
 N_CONTAINERS_RUNNING=$(docker ps --filter "status=running" | grep vulnerability-assessment-tool | awk '{print $1}' | wc -l)
 PATCH_ANALYZER_CREATED=$(docker images | grep vulnerability-assessment-tool-patch-analyzer | awk '{print $1}' | wc -l)
 
-if [ "${N_CONTAINERS_RUNNING}" -eq 8 ] && [ "${PATCH_ANALYZER_CREATED}" -eq 1 ]
+if [ "${N_CONTAINERS_RUNNING}" -eq 9 ] && [ "${PATCH_ANALYZER_CREATED}" -eq 1 ]
 then
     exit 0
 else
