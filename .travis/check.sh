f5677c232530255059c9f44ee60302090a9e73b5
sumeetpatil <sumeet.patil@sap.com>
2020-10-02 01:02:27 +0200

    renamed docker images

--- .travis/check.sh
+++ .travis/check.sh
@@ -5,10 +5,10 @@ set -x
 
 sleep 15
 
-N_CONTAINERS_RUNNING=$(docker ps --filter "status=running" | grep vulnerability-assessment-tool | awk '{print $1}' | wc -l)
-PATCH_ANALYZER_CREATED=$(docker images | grep vulnerability-assessment-tool-patch-analyzer | awk '{print $1}' | wc -l)
+N_CONTAINERS_RUNNING=$(docker ps --filter "status=running" | grep steady | awk '{print $1}' | wc -l)
+REST_BACKEND_CREATED=$(docker images | grep steady-rest-backend | awk '{print $1}' | wc -l)
 
-if [ "${N_CONTAINERS_RUNNING}" -eq 9 ] && [ "${PATCH_ANALYZER_CREATED}" -eq 1 ]
+if [ "${N_CONTAINERS_RUNNING}" -eq 9 ] && [ "${REST_BACKEND_CREATED}" -eq 1 ]
 then
     exit 0
 else
