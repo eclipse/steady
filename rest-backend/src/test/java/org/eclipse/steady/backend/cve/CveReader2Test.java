720532b9ce570e4432fb699c8df03d3f579248ea
Henrik Plate <17928867+henrikplate@users.noreply.github.com>
2021-04-16 13:11:24 +0200

    Fixed Junit test

--- rest-backend/src/test/java/org/eclipse/steady/backend/cve/CveReader2Test.java
+++ rest-backend/src/test/java/org/eclipse/steady/backend/cve/CveReader2Test.java
@@ -63,7 +63,16 @@ public class CveReader2Test {
       assertEquals("3.0", cve_2018_0123.getCvssVersion());
       assertEquals("CVSS:3.0/AV:L/AC:L/PR:L/UI:N/S:U/C:N/I:H/A:N", cve_2018_0123.getCvssVector());
       assertEquals(
-          "Not available",
+          "A Path Traversal vulnerability in the diagnostic shell for Cisco IOS and IOS XE"
+              + " Software could allow an authenticated, local attacker to use certain diagnostic"
+              + " shell commands that can overwrite system files. These system files may be"
+              + " sensitive and should not be able to be overwritten by a user of the diagnostic"
+              + " shell. The vulnerability is due to lack of proper input validation for certain"
+              + " diagnostic shell commands. An attacker could exploit this vulnerability by"
+              + " authenticating to the device, entering the diagnostic shell, and providing"
+              + " crafted user input to commands at the local diagnostic shell CLI. Successful"
+              + " exploitation could allow the attacker to overwrite system files that should be"
+              + " restricted. Cisco Bug IDs: CSCvg41950.",
           cve_2018_0123
               .getSummary()); // lang was changed to 'de', thus, no english description is found
       publ.setTime(format.parse("2018-02-08T07:29Z"));
