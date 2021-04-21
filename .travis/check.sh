1f62c4d26b90e6aa07aab76e33d6221c5464918b
Sumeet Patil <sumeet.patil@sap.com>
2021-04-21 05:46:03 -0400

    Fix travis job for releasing pipeline image
    Signed-off-by: Sumeet Patil <sumeet.patil@sap.com>

--- .travis/check.sh
+++ .travis/check.sh
@@ -7,8 +7,9 @@ sleep 15
 
 N_CONTAINERS_RUNNING=$(docker ps --filter "status=running" | grep steady | awk '{print $1}' | wc -l)
 REST_BACKEND_CREATED=$(docker images | grep steady-rest-backend | awk '{print $1}' | wc -l)
+STEADY_PIPELINE_CREATED=$(docker images | grep steady-pipeline | awk '{print $1}' | wc -l)
 
-if [ "${N_CONTAINERS_RUNNING}" -eq 11 ] && [ "${REST_BACKEND_CREATED}" -eq 1 ]
+if [ "${N_CONTAINERS_RUNNING}" -eq 10 ] && [ "${REST_BACKEND_CREATED}" -eq 1 ] && [ "${STEADY_PIPELINE_CREATED}" -eq 1 ]
 then
     exit 0
 else
