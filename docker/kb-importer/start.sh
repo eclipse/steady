08defd1d62acaea2e8f60ec6f7c84ec7581f7ec6
henrikplate <17928867+henrikplate@users.noreply.github.com>
2021-06-24 17:36:32 +0200

    Removed use of rest-nvd by kb-importer

--- docker/kb-importer/start.sh
+++ docker/kb-importer/start.sh
@@ -3,7 +3,7 @@
 #kaybee update
 mkdir -p /kb-importer/data
 cd /kb-importer/data
-mv /kb-importer/kb-importer.jar /kb-importer/kaybee /kb-importer/data
+cp /kb-importer/kb-importer.jar /kb-importer/kaybee /kb-importer/data
 ./kaybee update --force
 
 #Adding certs
