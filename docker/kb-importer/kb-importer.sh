b2523114174b7ac9578b64949477a94f66da8f10
serenaponta <42769540+serenaponta@users.noreply.github.com>
2021-06-24 18:41:08 +0200

    add lock to kaybee run; skip kaybee merge; write log

--- docker/kb-importer/kb-importer.sh
+++ docker/kb-importer/kb-importer.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
-#kaybee update
-cd /kb-importer/data
-./kaybee update --force
-
-#run kaybee import for kaybeeconf.yaml
-if [ -f /kb-importer/data/init ]
+if [ ! -f /kb-importer/data/running ]
 then
-  echo `date` " Running Kaybee Import"
+  touch /kb-importer/data/running
+
+  #kaybee update
+  cd /kb-importer/data
+  ./kaybee update --force
+
+  #run kaybee import for kaybeeconf.yaml
+  echo `date` " Running Kaybee Import" >> job.log 2>&1
   ./kaybee pull -c ../conf/kaybeeconf.yaml
-  echo `date` " Running Kaybee Merge"
-  ./kaybee merge -c ../conf/kaybeeconf.yaml
-  echo `date` " Kaybee Merge Done"
-  ./kaybee export -t steady -c ../conf/kaybeeconf.yaml
+#  echo `date` " Running Kaybee Merge" >> job.log 2>&1
+#  ./kaybee merge -s -c ../conf/kaybeeconf.yaml
+#  echo `date` " Kaybee Merge Done" >> job.log 2>&1
+  ./kaybee export -t steady -c ../conf/kaybeeconf.yaml -f .kaybee/repositories/github.com_sap.project-kb_vulnerability-data/statements/
   chmod +x steady.sh
-  sh steady.sh
-  echo `date` " Kaybee Import Done"
+  sh steady.sh >> job.log 2>&1
+  echo `date` " Kaybee Import Done" >> job.log 2>&1
+  rm /kb-importer/data/running
+else
+  echo `date` " Kaybee Import already Running" >> job.log 2>&1
 fi
