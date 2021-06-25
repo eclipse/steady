b61a6b0231acbb8a04fafcacaf73c9100490ea5b
serenaponta <42769540+serenaponta@users.noreply.github.com>
2021-06-25 15:32:36 +0200

    configurable repo, branch, folder for kaybee pull (skipping merge)

--- docker/kb-importer/kb-importer.sh
+++ docker/kb-importer/kb-importer.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ ! -f /kb-importer/data/running ]
 then
@@ -8,13 +8,15 @@ then
   cd /kb-importer/data
   ./kaybee update --force
 
-  #run kaybee import for kaybeeconf.yaml
+  #run kaybee import for kaybeeconf-eval.yaml (as it contains the substituted env variables for the source repo and branch)
   echo `date` " Running Kaybee Import" 
-  ./kaybee pull -c ../conf/kaybeeconf.yaml
+  ./kaybee pull -c ../conf/kaybeeconf-eval.yaml
+# As we cannot configure the destination folder of kaybee pull (for now), we explicitly move the resulting folder to the configured one and skip kaybee merge as we only have 1 source configurable
+  mv .kaybee/repositories/github.com_sap.project-kb_$KB_IMPORTER_STATEMENTS_BRANCH/statements $KB_IMPORTER_STATEMENTS_FOLDER
 #  echo `date` " Running Kaybee Merge" >> job.log 2>&1
 #  ./kaybee merge -s -c ../conf/kaybeeconf.yaml
 #  echo `date` " Kaybee Merge Done" >> job.log 2>&1
-  ./kaybee export -t steady -c ../conf/kaybeeconf.yaml -f .kaybee/repositories/github.com_sap.project-kb_vulnerability-data/statements/
+  ./kaybee export -t steady -c ../conf/kaybeeconf.yaml -f $KB_IMPORTER_STATEMENTS_FOLDER
   chmod +x steady.sh
   ./steady.sh 
   echo `date` " Kaybee Import Done" 
