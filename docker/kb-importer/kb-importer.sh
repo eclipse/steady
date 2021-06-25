7ef9b7f4dc6a60267ecc25732a25dd2def209923
serenaponta <42769540+serenaponta@users.noreply.github.com>
2021-06-25 13:37:06 +0200

    remove part of redirected log

--- docker/kb-importer/kb-importer.sh
+++ docker/kb-importer/kb-importer.sh
@@ -9,16 +9,16 @@ then
   ./kaybee update --force
 
   #run kaybee import for kaybeeconf.yaml
-  echo `date` " Running Kaybee Import" >> job.log 2>&1
+  echo `date` " Running Kaybee Import" 
   ./kaybee pull -c ../conf/kaybeeconf.yaml
 #  echo `date` " Running Kaybee Merge" >> job.log 2>&1
 #  ./kaybee merge -s -c ../conf/kaybeeconf.yaml
 #  echo `date` " Kaybee Merge Done" >> job.log 2>&1
   ./kaybee export -t steady -c ../conf/kaybeeconf.yaml -f .kaybee/repositories/github.com_sap.project-kb_vulnerability-data/statements/
   chmod +x steady.sh
-  ./steady.sh >> job.log 2>&1
-  echo `date` " Kaybee Import Done" >> job.log 2>&1
+  ./steady.sh 
+  echo `date` " Kaybee Import Done" 
   rm /kb-importer/data/running
 else
-  echo `date` " Kaybee Import already Running" >> job.log 2>&1
+  echo `date` " Kaybee Import already Running" 
 fi
