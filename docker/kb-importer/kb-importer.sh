2c92c4a971d3cd9f8f58f179c6be5e9f56a8a194
serenaponta <42769540+serenaponta@users.noreply.github.com>
2021-06-25 16:51:17 +0200

    use cp instead of mv (in place of kaybee merge

--- docker/kb-importer/kb-importer.sh
+++ docker/kb-importer/kb-importer.sh
@@ -11,8 +11,8 @@ then
   #run kaybee import for kaybeeconf-eval.yaml (as it contains the substituted env variables for the source repo and branch)
   echo `date` " Running Kaybee Import" 
   ./kaybee pull -c ../conf/kaybeeconf-eval.yaml
-# As we cannot configure the destination folder of kaybee pull (for now), we explicitly move the resulting folder to the configured one and skip kaybee merge as we only have 1 source configurable
-  mv .kaybee/repositories/github.com_sap.project-kb_$KB_IMPORTER_STATEMENTS_BRANCH/statements $KB_IMPORTER_STATEMENTS_FOLDER
+# As we cannot configure the destination folder of kaybee pull (for now), we explicitly copy the resulting folder to the configured one and skip kaybee merge as we only have 1 source configurable
+  cp -r .kaybee/repositories/github.com_sap.project-kb_$KB_IMPORTER_STATEMENTS_BRANCH/statements/. $KB_IMPORTER_STATEMENTS_FOLDER/
 #  echo `date` " Running Kaybee Merge" >> job.log 2>&1
 #  ./kaybee merge -s -c ../conf/kaybeeconf.yaml
 #  echo `date` " Kaybee Merge Done" >> job.log 2>&1
