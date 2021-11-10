ec69c95aa3bc969124e296fd0891649aea71e7b5
serenaponta <42769540+serenaponta@users.noreply.github.com>
2021-11-10 19:23:19 +0100

    do not save app after debloating

--- lang/src/main/java/org/eclipse/steady/goals/DebloatGoal.java
+++ lang/src/main/java/org/eclipse/steady/goals/DebloatGoal.java
@@ -86,56 +86,56 @@ public class DebloatGoal extends AbstractAppGoal {
     }
 
     // Upload libraries and binaries (if requested)
-    if (a.getDependencies() != null) {
-      for (Dependency dep : a.getDependencies()) {
+//    if (a.getDependencies() != null) {
+//      for (Dependency dep : a.getDependencies()) {
+//
+//        // Upload lib
+//        final Library lib = dep.getLib();
+//        if (lib != null) {
+//          if (lib.hasValidDigest()) {
+//            BackendConnector.getInstance().uploadLibrary(this.getGoalContext(), lib);
+//            if (CoreConfiguration.isJarUploadEnabled(this.getGoalContext().getVulasConfiguration()))
+//              BackendConnector.getInstance()
+//                  .uploadLibraryFile(lib.getDigest(), Paths.get(dep.getPath()));
+//          } else {
+//            log.error("Library of dependency [" + dep + "] has no valid digest");
+//          }
+//        } else {
+//          log.error("Dependency [" + dep + "] has no library");
+//        }
+//      }
+//    }
 
-        // Upload lib
-        final Library lib = dep.getLib();
-        if (lib != null) {
-          if (lib.hasValidDigest()) {
-            BackendConnector.getInstance().uploadLibrary(this.getGoalContext(), lib);
-            if (CoreConfiguration.isJarUploadEnabled(this.getGoalContext().getVulasConfiguration()))
-              BackendConnector.getInstance()
-                  .uploadLibraryFile(lib.getDigest(), Paths.get(dep.getPath()));
-          } else {
-            log.error("Library of dependency [" + dep + "] has no valid digest");
-          }
-        } else {
-          log.error("Dependency [" + dep + "] has no library");
-        }
-      }
-    }
-
-    final boolean upload_empty =
-        this.getConfiguration()
-            .getConfiguration()
-            .getBoolean(CoreConfiguration.APP_UPLOAD_EMPTY, false);
-    final boolean app_exists_in_backend =
-        BackendConnector.getInstance().isAppExisting(this.getGoalContext(), a);
-
-    // Upload if non-empty or already exists in backend or empty ones shall be uploaded
-    if (!a.isEmpty() || app_exists_in_backend || upload_empty) {
-      log.info(
-          "Save app "
-              + a
-              + " with ["
-              + a.getDependencies().size()
-              + "] dependencies and ["
-              + a.getConstructs().size()
-              + "] constructs (uploadEmpty="
-              + upload_empty
-              + ")");
-      BackendConnector.getInstance().uploadApp(this.getGoalContext(), a);
-    } else {
-      log.warn(
-          "Skip save of empty app "
-              + this.getApplicationContext()
-              + " (uploadEmpty="
-              + upload_empty
-              + ", existsInBackend="
-              + app_exists_in_backend
-              + ")");
-      this.skipGoalUpload();
-    }
+//    final boolean upload_empty =
+//        this.getConfiguration()
+//            .getConfiguration()
+//            .getBoolean(CoreConfiguration.APP_UPLOAD_EMPTY, false);
+//    final boolean app_exists_in_backend =
+//        BackendConnector.getInstance().isAppExisting(this.getGoalContext(), a);
+//
+//    // Upload if non-empty or already exists in backend or empty ones shall be uploaded
+//    if (!a.isEmpty() || app_exists_in_backend || upload_empty) {
+//      log.info(
+//          "Save app "
+//              + a
+//              + " with ["
+//              + a.getDependencies().size()
+//              + "] dependencies and ["
+//              + a.getConstructs().size()
+//              + "] constructs (uploadEmpty="
+//              + upload_empty
+//              + ")");
+//      BackendConnector.getInstance().uploadApp(this.getGoalContext(), a);
+//    } else {
+//      log.warn(
+//          "Skip save of empty app "
+//              + this.getApplicationContext()
+//              + " (uploadEmpty="
+//              + upload_empty
+//              + ", existsInBackend="
+//              + app_exists_in_backend
+//              + ")");
+//      this.skipGoalUpload();
+//    }
   }
 }
