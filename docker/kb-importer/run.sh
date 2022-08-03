727698e601936fd6acf13ed66b24623d26a61043
henrikplate <17928867+henrikplate@users.noreply.github.com>
2022-08-03 15:04:00 +0200

    Changed Docker Compose so that start-steady works in clone and after setup-steady

--- docker/kb-importer/run.sh
+++ docker/kb-importer/run.sh
@@ -6,8 +6,8 @@ if [ -f /kb-importer/kb-importer.jar ]; then
   mv /kb-importer/kb-importer.jar /kb-importer/kaybee /kb-importer/data
 fi
 
-#substitute env variables used by kaybee in kaybeeconf.yaml
-sed "s|KB_IMPORTER_STATEMENTS_REPO|$KB_IMPORTER_STATEMENTS_REPO|g" ../conf/kaybeeconf.yaml.sample > ../conf/kaybeeconf.yaml
+# Substitute env variables used by kaybee in kaybeeconf.yaml
+sed    "s|KB_IMPORTER_STATEMENTS_REPO|$KB_IMPORTER_STATEMENTS_REPO|g"     ../conf/kaybeeconf.yaml.sample > ../conf/kaybeeconf.yaml
 sed -i "s|KB_IMPORTER_STATEMENTS_BRANCH|$KB_IMPORTER_STATEMENTS_BRANCH|g" ../conf/kaybeeconf.yaml
 
 echo "Statements repo: " $KB_IMPORTER_STATEMENTS_REPO
@@ -16,18 +16,13 @@ echo "Statements branch: " $KB_IMPORTER_STATEMENTS_BRANCH
 echo "Statements folder: " $KB_IMPORTER_STATEMENTS_FOLDER
 echo "Skip clones: " $KB_IMPORTER_SKIP_CLONE
 
-#Adding certs
+# Adding certs
 certs=`ls /kb-importer/certs | grep -v readme.txt`
 for cert in $certs; do
    keytool -import -alias $cert -storepass changeit -keystore /usr/lib/jvm/java-1.8-openjdk/jre/lib/security/cacerts -file /kb-importer/certs/$cert -noprompt
 done
 
-(java -Dvulas.shared.backend.header.X-Vulas-Client-Token=$BACKEND_BUGS_TOKEN \
-            -Dvulas.shared.cia.serviceUrl=$CIA_SERVICE_URL \
-            -Dvulas.shared.backend.serviceUrl=$BACKEND_SERVICE_URL \
-            -jar /kb-importer/data/kb-importer.jar | tee analyzer_logs.txt ) &
-
-#Wait for kb-importer and backend to start
-sleep 40
-
-curl localhost:8080/start?skipClone=$KB_IMPORTER_SKIP_CLONE -X POST
+java -Dvulas.shared.backend.header.X-Vulas-Client-Token=$BACKEND_BUGS_TOKEN \
+          -Dvulas.shared.cia.serviceUrl=$CIA_SERVICE_URL \
+          -Dvulas.shared.backend.serviceUrl=$BACKEND_SERVICE_URL \
+          -jar /kb-importer/data/kb-importer.jar | tee analyzer_logs.txt
--- docker/patch-lib-analyzer/run.sh
+++ docker/patch-lib-analyzer/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Wait for backend to start and kb-importer to insert data
+# Wait for backend to start and kb-importer to insert data
 sleep 300
 
 java \
@@ -10,4 +10,4 @@ java \
 	-Dhttps.proxyHost=$HTTPS_PROXY_HOST \
 	-Dhttps.proxyPort=$HTTPS_PROXY_PORT \
     -Dspring.profiles.active=docker \
-	-jar /vulas/patch-lib-analyzer.jar $PATCHEVAL_OPTS
+	-jar /steady/patch-lib-analyzer.jar $PATCHEVAL_OPTS
