29636dfe636a5eccd9f23a9fe4b6954e1ed9eb31
henrikplate <17928867+henrikplate@users.noreply.github.com>
2022-08-03 18:16:29 +0200

    Avoid mv of binaries into data folder

--- docker/kb-importer/run.sh
+++ docker/kb-importer/run.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-mkdir -p /kb-importer/data
-cd /kb-importer/data
-if [ -f /kb-importer/kb-importer.jar ]; then
-  mv /kb-importer/kb-importer.jar /kb-importer/kaybee /kb-importer/data
-fi
+# Home directory of kb-importer
+home="/kb-importer"
 
-# Substitute env variables used by kaybee in kaybeeconf.yaml
-sed    "s|KB_IMPORTER_STATEMENTS_REPO|$KB_IMPORTER_STATEMENTS_REPO|g"     ../conf/kaybeeconf.yaml.sample > ../conf/kaybeeconf.yaml
-sed -i "s|KB_IMPORTER_STATEMENTS_BRANCH|$KB_IMPORTER_STATEMENTS_BRANCH|g" ../conf/kaybeeconf.yaml
+mkdir -p $home/data
 
 echo "Statements repo: " $KB_IMPORTER_STATEMENTS_REPO
-
 echo "Statements branch: " $KB_IMPORTER_STATEMENTS_BRANCH
 echo "Statements folder: " $KB_IMPORTER_STATEMENTS_FOLDER
 echo "Skip clones: " $KB_IMPORTER_SKIP_CLONE
 
+# Substitute env variables used by kaybee in kaybeeconf.yaml
+sed    "s|KB_IMPORTER_STATEMENTS_REPO|$KB_IMPORTER_STATEMENTS_REPO|g"     $home/conf/kaybeeconf.yaml.sample > $home/conf/kaybeeconf.yaml
+sed -i "s|KB_IMPORTER_STATEMENTS_BRANCH|$KB_IMPORTER_STATEMENTS_BRANCH|g" $home/conf/kaybeeconf.yaml
+
 # Adding certs
-certs=`ls /kb-importer/certs | grep -v readme.txt`
+certs=`ls $home/certs | grep -v readme.txt`
 for cert in $certs; do
-   keytool -import -alias $cert -storepass changeit -keystore /usr/lib/jvm/java-1.8-openjdk/jre/lib/security/cacerts -file /kb-importer/certs/$cert -noprompt
+   keytool -import -alias $cert -storepass changeit -keystore /usr/lib/jvm/java-1.8-openjdk/jre/lib/security/cacerts -file $home/certs/$cert -noprompt
 done
 
 java -Dvulas.shared.backend.header.X-Vulas-Client-Token=$BACKEND_BUGS_TOKEN \
           -Dvulas.shared.cia.serviceUrl=$CIA_SERVICE_URL \
           -Dvulas.shared.backend.serviceUrl=$BACKEND_SERVICE_URL \
-          -jar /kb-importer/data/kb-importer.jar | tee analyzer_logs.txt
+          -jar $home/kb-importer.jar | tee $home/data/analyzer_logs.txt
